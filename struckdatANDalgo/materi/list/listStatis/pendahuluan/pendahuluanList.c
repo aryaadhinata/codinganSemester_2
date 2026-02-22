ae8da791337441fc94ef53ca89abbb9faeae9af2
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-02-22 11:14:35 +0700

    bagian cek indeks yang kosong elementnya clear

--- struckdatANDalgo/materi/list/listStatis/pendahuluan/pendahuluanList.c
+++ struckdatANDalgo/materi/list/listStatis/pendahuluan/pendahuluanList.c
@@ -33,6 +33,7 @@ void createList(list *L){
     }
 }
 
+// menghitung banyak elemen yang tidak kosong
 int countElement(list L){
     int hasil = 0;
     if(L.first != -1){// cek apakah list pertama kosong atau tidak
@@ -53,6 +54,26 @@ int countElement(list L){
 
     // mengembalikan nilai hasil penambahan
     return hasil;
+}
+
+int emptyElement(list L){
+    int hasil = -1; // nilai indeks awal, nantinya akan disisi dengan indeks list yang masih kosong
+
+    if(countElement(L) < 10){ // selama masih belum melakukn iterasi kurang dari sepuluh
+        int ketemu = 0; // penanda jika sudah ketemu atau belum
+
+        int i = 0; // iterator
+        while((ketemu == 0)&&(i < 10)){ // looping selama belum menmukan yang kosong dan belum melalui semua list
+            if(L.data[i].next == -2){ // jika nilai list berikutnya -2 maka list berikutny itu kosong
+                hasil = i; // indeks sekarang adalah indeksnya
+                ketemu = 1; // penanda agar keluar dari loop
+            }else{ // tambah iterasi lagi
+                i = i + 1;
+            }
+        }
+    }
+    
+    return hasil; // mengembalikan nilai ideks hasil yang kosong
 }
 
 int main(){
